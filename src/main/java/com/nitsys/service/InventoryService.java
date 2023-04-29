dd84f33a5f29806bceb5d4f6a4038811bd4d10a5
nitin pathak <nitin.pathak05@gmail.com>
2023-04-29 20:55:16 +0530

    commented error due partial developed functionality

--- src/main/java/com/nitsys/service/InventoryService.java
+++ src/main/java/com/nitsys/service/InventoryService.java
@@ -10,12 +10,15 @@ import com.nitsys.dto.Product;
 @Service
 public class InventoryService {
 	
-	public List<Product> getAllProduct(){
-		
-		return Stream.of(new Product(1,"Mobile",5000.32),new Product(2, "TV", 10000.54)).collect(null);
-		
-		 
-		
-	}
+	/*
+	 * public List<Product> getAllProduct(){
+	 * 
+	 * return Stream.of(new Product(1,"Mobile",5000.32),new Product(2, "TV",
+	 * 10000.54)).collect(null);
+	 * 
+	 * 
+	 * 
+	 * }
+	 */
 
 }
